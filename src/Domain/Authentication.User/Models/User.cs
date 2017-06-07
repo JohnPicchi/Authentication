1036c9cb9c8e5261779ad0764a15706cf5e00209
John Picchi <john.picchi@gmail.com>
2017-06-06 23:37:20 -0400

    -Added Authentication.User to source control

---
+++ src/Domain/Authentication.User/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Authentication.Domain;
+
+namespace Authentication.User.Models
+{
+  public class User : DomainEntity<Guid>
+  {
+    public User()
+    {
+      
+    }
+
+    public delegate User Factory();
+
+    public virtual string FirstName { get; set; }
+
+    public virtual string LastName { get; set; }
+
+    public virtual string Email { get; set; }
+
+    public virtual string PhoneNumber { get; set; }
+
+    public string FullName => String.Join(" ", FirstName, LastName);
+  }
+}
