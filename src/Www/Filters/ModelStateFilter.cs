e4ed1f90c0e0cabb56516d32ce1dbf9dc0b98b33
John Picchi <john.picchi@gmail.com>
2017-06-15 01:57:39 -0400

    ModelStateFIlter refactored

--- src/Www/Filters/ModelStateFilter.cs
+++ src/Www/Filters/ModelStateFilter.cs
@@ -21,27 +21,7 @@ namespace Authentication.Filters
       if (!context.ModelState.IsValid)
       {
         var controller = (Controller) context.Controller;
-
-        controller.HttpContext.Response.StatusCode = (int) HttpStatusCode.BadRequest;
-
-        var actionName = controller.ControllerContext.ActionDescriptor.ActionName;
-        var viewEngineResult = viewEngine.FindView(controller.Url.ActionContext, actionName, false);
-
-        if (viewEngineResult.Success)
-        {
-          var viewResult = new ViewResult
-          {
-            ViewName = viewEngineResult.ViewName,
-            ViewData = new ViewDataDictionary(controller.MetadataProvider, context.ModelState)
-          };
-          //Add an error message????
-          //viewResult.ViewData.Add("","An Error Occurred");
-          context.Result = viewResult;
-        }
-        else
-        {
-          context.Result = new BadRequestResult();
-        }
+        await new ViewResult().ExecuteResultAsync(controller.Url.ActionContext);
       }
       else
         await next();
