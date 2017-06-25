43feca17e9779982a595ff2223bcae92fe63e388
John Picchi <john.picchi@gmail.com>
2017-06-25 19:58:10 -0400

    ...

---
+++ src/Domain/Authentication.User/PersistenceModels/UserPersistenceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Authentication.User.PersistenceModels
+{
+  public class UserPersistenceModel : IdentityUser<Guid, IdentityUserClaim<Guid>, IdentityUserRole<Guid>, UserLoginPersistenceModel>
+  {
+
+  }
+}
