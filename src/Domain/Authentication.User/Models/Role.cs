a3657118861161390a55a12189d3bad7b745188f
John Picchi <john.picchi@gmail.com>
2017-07-07 22:42:55 -0400

    -Removed some unnecessary classes -Started Admin controller -Added Untracked files

---
+++ src/Domain/Authentication.User/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Authentication.User.Models
+{
+  public class Role : IdentityRole<Guid>
+  {
+  }
+}
---
+++ src/Domain/Authentication.User/Models/UserAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authentication.User.Models
+{
+  public class UserAddress
+  {
+    public Guid Id { get; set; }
+
+    public User User { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string AddressLine1 { get; set; }
+
+    public string AddressLine2 { get; set; }
+
+    public string PostalCode { get; set; }
+
+    public string StateProvinceRegion { get; set; }
+
+    public string Country { get; set; }
+  }
+}
